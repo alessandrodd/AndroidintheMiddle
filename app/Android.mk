a262e8f39e5ec503d72ef2ee2a71b4934a2c0f31
didiego.alessandro <didiego.alessandro@yahoo.com>
2016-08-11 19:37:50 +0200

    Started to import and use external C libraries (libpcap)

---
+++ app/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+MY_LOCAL_PATH := $(LOCAL_PATH)/src/main/cpp
+
+include $(CLEAR_VARS)
+
+include $(MY_LOCAL_PATH)/hello-cpp/Android.mk
+#include $(MY_LOCAL_PATH)/androdump/Android.mk
+#include $(MY_LOCAL_PATH)/arpspoof/Android.mk
+#include $(MY_LOCAL_PATH)/host-detection/Android.mk
+#include $(MY_LOCAL_PATH)/dns-spoofing/Android.mk
+#include $(MY_LOCAL_PATH)/libpcap/Android.mk
